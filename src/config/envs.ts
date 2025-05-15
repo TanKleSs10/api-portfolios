2ea7e1a4ac840b1d54493f53a91f491236cad6c8
Diego Meza <diegoerik_mh@hotmail.com>
2025-05-15 18:55:13 +0000

    Refactor:  Is added new env for email service

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,10 +2,17 @@ import { get } from "env-var";
 
 
 export const envs = {
-    MONGODB_URI: get("MONGODB_URI").required().asString(),
-    PORT: get("PORT").required().asPortNumber(),
     ENV: get("ENV").required().asString(),
+    PORT: get("PORT").required().asPortNumber(),
+    MONGODB_URI: get("MONGODB_URI").required().asString(),
+    
     CLOUDINARY_CLOUD_NAME: get("CLOUDINARY_CLOUD_NAME").required().asString(),
     CLOUDINARY_API_KEY: get("CLOUDINARY_API_KEY").required().asString(),
     CLOUDINARY_API_SECRET: get("CLOUDINARY_API_SECRET").required().asString(),
+    
+    JWT_SEED: get("JWT_SEED").required().asString(),
+
+    MAILER_EMAIL: get("MAILER_EMAIL").required().asString(),
+    MAILER_SECRET: get("MAILER_SECRET").required().asString(),
+    MAILER_SERVICE: get("MAILER_SERVICE").required().asString(),
 };
